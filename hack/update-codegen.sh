6ec252cb3a5a0c8e415c24624fe483726c7b252b
Markus Thömmes <markusthoemmes@me.com>
2019-10-25 08:48:48 +0200

    Generate Maistra clients.

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -14,4 +14,19 @@ EXTERNAL_INFORMER_PKG="github.com/openshift/client-go/route/informers/externalve
     "route:v1" \
     --go-header-file hack/boilerplate.txt
 
-dep ensure -v
+# Generate maistra clients.
+vendor/k8s.io/code-generator/generate-groups.sh "client,informer,lister" \
+  github.com/openshift-knative/knative-serving-networking-openshift/pkg/client \
+  github.com/maistra/istio-operator/pkg/apis \
+  "maistra:v1" \
+  --go-header-file hack/boilerplate.txt
+
+# Generate maistra injection code.
+OUTPUT_PKG="github.com/openshift-knative/knative-serving-networking-openshift/pkg/client/injection/maistra" \
+vendor/knative.dev/pkg/hack/generate-knative.sh "injection" \
+  github.com/openshift-knative/knative-serving-networking-openshift/pkg/client \
+  github.com/maistra/istio-operator/pkg/apis \
+  "maistra:v1" \
+  --go-header-file hack/boilerplate.txt
+
+hack/update-deps.sh
---
+++ vendor/github.com/maistra/istio-operator/pkg/apis/maistra/v1/register.go
@@ -0,0 +1,52 @@
+// NOTE: Boilerplate only.  Ignore this file.
+
+// Package v1 contains API Schema definitions for the maistra v1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=maistra.io
+package v1
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sigs.k8s.io/controller-runtime/pkg/runtime/scheme"
+)
+
+var (
+	// SchemeGroupVersion is group version used to register these objects
+	SchemeGroupVersion = schema.GroupVersion{Group: "maistra.io", Version: "v1"}
+
+	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
+	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
+)
+
+var (
+	GroupName     = SchemeGroupVersion.Group
+	GroupVersion  = schema.GroupVersion{Group: GroupName, Version: SchemeGroupVersion.Version}
+	schemeBuilder = runtime.NewSchemeBuilder(addKnownTypes, corev1.AddToScheme)
+	// Install is a function which adds this version to a scheme
+	Install = schemeBuilder.AddToScheme
+
+	// AddToScheme exists solely to keep the old generators creating valid code
+	// DEPRECATED
+	AddToScheme = schemeBuilder.AddToScheme
+)
+
+// Resource generated code relies on this being here, but it logically belongs to the group
+// DEPRECATED
+func Resource(resource string) schema.GroupResource {
+	return schema.GroupResource{Group: GroupName, Resource: resource}
+}
+
+// Adds the list of known types to api.Scheme.
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(GroupVersion,
+		&ServiceMeshMemberRoll{},
+		&ServiceMeshMemberRollList{},
+		&ServiceMeshControlPlane{},
+		&ServiceMeshControlPlaneList{},
+	)
+	metav1.AddToGroupVersion(scheme, GroupVersion)
+	return nil
+}
